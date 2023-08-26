d8c18ddf59b4aa43070add47b0ac7bc7f2ace4fb
ArchLeaders <archleadership28@gmail.com>
2023-08-26 11:06:45 -0700

    Impl TotkPath extension

---
+++ src/Helpers/TotkPathExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace Totk.HashCalculator.Helpers;
+
+public static class TotkPathExtension
+{
+    public static string Canonical(this string path)
+    {
+        return (path.EndsWith(".zs") || path.EndsWith(".mc") ? path[..(^3)] : path).Replace('\\', '/');
+    }
+}
